4c20039c4c38c4f367cfec81b1b3384677ad7849
Grace <gmk1914@yahoo.com>
2018-02-20 09:39:40 -0600

    //

--- app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/Disk.java
+++ app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/Disk.java
@@ -8,22 +8,37 @@ public class Disk
 {
     private int size;
     private Disk nextDisk;
+    private String display;
 
     public Disk(int size)
     {
         this.size = size;
         this.nextDisk = null;
+        for(int i = 0; i < size; i++)
+        {
+            this.display = this.display + "=";
+        }
     }
 
-    public int getSize() {
+    public int getSize()
+    {
+
         return size;
     }
 
-    public Disk getNextDisk() {
+    public Disk getNextDisk()
+    {
         return nextDisk;
     }
 
-    public void setNextDisk(Disk nextDisk) {
+
+    public void setNextDisk(Disk nextDisk)
+    {
         this.nextDisk = nextDisk;
+    }
+
+    public void display()
+    {
+        System.out.println(display);
     }
 }
--- app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/MainActivity.java
+++ app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.awesomefat.towerofhanoi_csc300_spring2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewGroup tower_1;
+    private ViewGroup tower_2;
+    private ViewGroup tower_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        this.tower_1 = (ViewGroup)this.findViewById(R.id.tower_1);
+        this.tower_2 = (ViewGroup)this.findViewById(R.id.tower_2);
+        this.tower_3 = (ViewGroup)this.findViewById(R.id.tower_3);
+    }
+
+    public void setTower_1Pressed(View v)
+    {
+
+            View tower = this.tower_1.getChildAt(0);
+            this.tower_1.removeViewAt(0);
+
+    }
+    public void setTower_2Pressed(View v)
+    {
+
+        View tower = this.tower_2.getChildAt(0);
+        this.tower_3.removeViewAt(0);
+
+    }
+
+    public void setTower_3Pressed(View v)
+    {
+
+        View tower = this.tower_1.getChildAt(0);
+        this.tower_3.removeViewAt(0);
+
     }
 }
